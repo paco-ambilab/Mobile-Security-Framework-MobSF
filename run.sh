e594590106adc2f34ce47703a6229390bd304d35
paco <paco89lol@gmail.com>
2020-07-12 18:23:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Dev Server
-. venv/bin/activate && python manage.py runserver 0.0.0.0:8000
+#. venv/bin/activate && python manage.py runserver 0.0.0.0:8000
 # Prod Server
-#. venv/bin/activate && gunicorn -b 0.0.0.0:8000 MobSF.wsgi:application --workers=1 --threads=10 --timeout=1800
+. venv/bin/activate && gunicorn -b 0.0.0.0:8000 MobSF.wsgi:application --workers=1 --threads=10 --timeout=1800
